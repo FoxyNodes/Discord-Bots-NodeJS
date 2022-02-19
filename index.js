1fc1b05bd86b00517cc86c04a4c7f6b2657f9061
RianOP <87343593+RianOP@users.noreply.github.com>
2022-02-19 15:58:24 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let fs = require("fs")
 
-const settings = require("./index.js");
+const server = require("./index.js");
     console.log("Welcome to your new FoxyNodes server!");
     console.log("Start by creating a index.js or uploading your files");
     console.log("You can edit this at ./index.js");
